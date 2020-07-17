e980d4fd887937524d14f39b7370ca7cd406b745
yaminishewale <53417213+yaminishewale@users.noreply.github.com>
2020-07-17 20:00:56 +0530

    Add files via upload

---
+++ day3/question3/question3.js
@@ -0,0 +1,21 @@
+console.log(" marks and grade ")
+
+var marks = prompt("enter a marks:");
+console.log(marks);
+
+   
+if(marks > 80)
+{
+  console.log(`the marks are ${marks} and grade is A`)
+}
+if(marks > 50)
+{
+  console.log(`the marks are ${marks} and grade is B`)
+}
+else
+{
+    console.log(`the marks are ${marks} and grade is C`)
+}
+
+
+
